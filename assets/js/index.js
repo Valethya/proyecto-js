748ba799f1001e0b5c51f245e4a6c9702e26d32e
Valentina Rojas <valenthinarojasr@gmail.com>
2022-08-23 13:29:34 -0400

    archivos minificados

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,15 @@
-let numbers=[]
-for(i=0;i<=100;i++){
-    
-    let num=i+(numbers[1])   
-    numbers.push(num)
-}
-
-for(const num of numbers){
-    sum=sum+num
-}
 
 
 
-// funciona
-let numbers=[]
-for(i=0;i<=100;i++){
-    numbers.push(i)
-    let sum=0
-    for(const num of numbers){
-        sum=sum+num
-    }
-}// 5050
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems,{});
+  });
+  console.log('hello')
+
+
+  let randomnum=[]
+  for(i=0;i<=5;i++){
+
+  }
+  console.log(randomnum)
