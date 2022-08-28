608d9779b88f968655cd2987a3fda571f1af6f51
Valentina Rojas <valenthinarojasr@gmail.com>
2022-08-28 19:15:35 -0400

    imagenes nuevas, svg, articulos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(elems, {});
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {});
+});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems,{});
-  });
-  console.log('hello')
-
-
-  let randomnum=[]
-  for(i=0;i<=5;i++){
-
-  }
-  console.log(randomnum)
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, {});
+});
