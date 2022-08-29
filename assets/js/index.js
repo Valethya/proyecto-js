db85d16729c982a2cc1cac5e3f73d75ebd01d9a5
Valentina Rojas <valenthinarojasr@gmail.com>
2022-08-28 20:44:35 -0400

    comente los margin de row para centrar con flex

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,8 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".fixed-action-btn");
   var instances = M.FloatingActionButton.init(elems, {});
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".carousel");
-  var instances = M.Carousel.init(elems, {});
-});
